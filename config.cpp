cc01c570442976fe4b3496e9a565d39f3186b327
Lv100Latios <69733882+Lv100Latios@users.noreply.github.com>
2021-10-26 00:07:58 -0400

    Add files via upload

---
+++ config.cpp
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//This mod is NOT to be used in a commercial product and falls under BI's APL-SA License
+//Do not redistribute this mod without explicit permission from me: https://steamcommunity.com/id/Lv100Cinderace
+//However, do feel free to use this mod for inspiration, though there are likely much more talented people than myself.
+//Additionally, big thanks to Diwako for a lot of modding and scripting support. This would not have been made without him.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+class CfgPatches
+{
+	class HL1Screams
+	{
+		//Scientis screams. AAAAAAAAAUUUUUUUGGGGGGHHHHHH!
+		name = "HL1 Screams";
+		author = "Lv100Latias";
+		url = "";
+		
+		units[] = {};
+		weapon[] = {};
+		requiredVersion = 2.06;
+		requiredAddons[] = {
+			"ace_fire"
+		};
+		
+	};
+};
+
+
+class CfgSounds
+{
+    ace_fire_scream_1 = {"hl1screams\Sounds\ScientistScream1.wss",1,1};
+	ace_fire_scream_2 = {"hl1screams\Sounds\ScientistScream2.wss",1,1};
+	ace_fire_scream_3 = {"hl1screams\Sounds\ScientistScream3.wss",1,1};
+	ace_fire_scream_4 = {"hl1screams\Sounds\ScientistScream4.wss",1,1};
+	ace_fire_scream_5 = {"hl1screams\Sounds\ScientistScream5.wss",1,1};
+	ace_fire_scream_6 = {"hl1screams\Sounds\ScientistScream6.wss",1,1};
+	ace_fire_scream_7 = {"hl1screams\Sounds\ScientistScream7.wss",1,1};
+	ace_fire_scream_8 = {"hl1screams\Sounds\ScientistScream8.wss",1,1};
+	ace_fire_scream_9 = {"hl1screams\Sounds\ScientistScream9.wss",1,1};
+	ace_fire_scream_10 = {"hl1screams\Sounds\ScientistScream10.wss",1,1};
+	ace_fire_scream_11 = {"hl1screams\Sounds\ScientistScream11.wss",1,1};
+	ace_fire_scream_12 = {"hl1screams\Sounds\ScientistScream12.wss",1,1};
+	ace_fire_scream_13 = {"hl1screams\Sounds\ScientistScream13.wss",1,1};
+	ace_fire_scream_14 = {"hl1screams\Sounds\ScientistScream14.wss",1,1};
+	ace_fire_scream_15 = {"hl1screams\Sounds\ScientistScream15.wss",1,1};
+};
